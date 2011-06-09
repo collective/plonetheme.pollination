5b6cff2a4b110c9f3a17ca694d5e58f889d9bb4f
Andrew Mleczko <andrew@mleczko.net>
2011-06-09 05:58:19 +0000

    update metadata

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.0'
+version = '0.1'
 
 setup(name='plonetheme.pollination',
       version=version,
       description="Pollination Theme",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
+      keywords='web zope plone theme diazo',
+      author='Irene Capatti',
+      author_email='irene.capatti@redturtle.it',
+      url='http://svn.plone.org/svn/collective/plonetheme.pollination',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plonetheme'],
@@ -25,13 +23,9 @@ setup(name='plonetheme.pollination',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
           'plone.app.theming',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+      entry_points={
+          'z3c.autoinclude.plugin':'target = plone',
+      },
+)
